3befcbb3c0950de1a91722b0cb4de07b30e2d665
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-03-21 11:33:01 +0100

    Ajout de nouvelles classes pour tester les modèles.

--- src/test/java/test/TestData.java
+++ src/test/java/test/TestData.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class TestCoordinates {
+public class TestData {
     @Test
     public void testExemple() {
         assertTrue(true);
