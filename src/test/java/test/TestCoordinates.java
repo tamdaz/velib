e3681cc4d2025c9feec263fab3a31aa169c38ac0
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-03-21 11:12:56 +0100

    Ajout d'une classe pour tester le fonctionnement de JUnit

---
+++ src/test/java/test/TestCoordinates.java
@@ -0,0 +1,12 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestCoordinates {
+    @Test
+    public void testSomething() {
+        assertTrue(true);
+    }
+}
