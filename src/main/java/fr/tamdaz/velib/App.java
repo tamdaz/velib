6b3a00f0bdca199819792f68703ae4d8416e7b0a
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-03-22 01:24:04 +0100

    Ajout de commentaires pour la classe App

--- src/main/java/fr/tamdaz/velib/App.java
+++ src/main/java/fr/tamdaz/velib/App.java
@@ -10,32 +10,53 @@ import javafx.fxml.FXMLLoader;
 import javafx.application.Application;
 
 /**
- * Point d'entrée de l'application Velib;
+ * Application Vélib.
  */
 public class App extends Application {
     private static Scene scene;
 
+    /**
+     * Point d'entrée de l'application.
+     * 
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
-        new Gateway().start();
+        Gateway.start();
         launch();
     }
 
+    /**
+     * Permet de lancer l'application graphique.
+     */
     @Override
+    @SuppressWarnings("exports")
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"), 1200, 800);
         stage.setScene(scene);
         stage.setTitle("Vélib - App de disponibilités de vélos et bornes");
-        stage.setMinWidth(1000);
-        stage.setMinHeight(700);
-        stage.setWidth(1000);
-        stage.setHeight(700);
+        stage.setMinWidth(1200);
+        stage.setMinHeight(800);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
+    /**
+     * Méthode qui permet de définir le fichier FXML à charger pour la vue.
+     * 
+     * @param fxml Fichier FXML
+     * @throws IOException
+     */
+    protected static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
+    /**
+     * Méthode statique qui permet de charger la vue.
+     * 
+     * @param fxml Fichier FXML
+     * @return
+     * @throws IOException
+     */
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
